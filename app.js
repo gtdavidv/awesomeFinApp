12bcf0bee05cafa88368f7459e116932fba4db15
Riaz Virani <riaz.n.virani@gmail.com>
2014-10-24 22:32:55 -0400

    Added error ajax call

--- app.js
+++ app.js
@@ -1,48 +1,53 @@
-simply.setText({
-  title: 'FinApp!',
-  body: 'No Pending Transactions'
-}, true);
+try {
+  simply.setText({
+    title: 'FinApp!',
+    body: 'No Pending Transactions'
+  }, true);
 
-function statusUpdate() {
-  ajax({ url: 'http://dorsk.powweb.com/finapp/message.php?token' + Pebble.getAccountToken() }, function(data){
-    if(data.length > 0) {
-      clearInterval(ajaxIntervalid);
-      authenticate_payment(data);
-    } else {
-      simply.off('singleClick');
-      simply.off('accelTap');
-    }
-  });
-};
-
-// ajax interval call
-var ajaxIntervalid  = setInterval(statusUpdate, 2000);
-
-function authenticate_payment(data) {
-  simply.body(data);
-
-  simply.on('singleClick', function(e) {
-    var combo = localStorage.getItem('combo') || '';
-    if (e.button === 'up') {
-      combo += 't';
-    } else if (e.button === 'select') {
-      combo += 'm';
-    } else if (e.button === 'down') {
-      combo += 'b';
-    }
-    localStorage.setItem('combo', combo);
-    var passwordRep = Array(combo.length() + 1).join("*");
-    simply.title("Enter secret");
-    simply.body(passwordRep);
-
-  });
-
-  simply.on('accelTap', function(e) {
-    var combo = localStorage.getItem('combo') || '';
-    ajax({ url: 'http://dorsk.powweb.com/finapp/process.php?token' + Pebble.getAccountToken() + '&combo=' + combo }, function(data){
-      simply.body(data);
+  function statusUpdate() {
+    ajax({ url: 'http://dorsk.powweb.com/finapp/message.php?token' + Pebble.getAccountToken() }, function (data) {
+      if (data.length > 0) {
+        clearInterval(ajaxIntervalid);
+        authenticate_payment(data);
+      } else {
+        simply.off('singleClick');
+        simply.off('accelTap');
+      }
     });
-    localStorage.setItem('combo', '');
-    ajaxIntervalid  = setInterval(statusUpdate, 2000);
-  });
+  }
+
+  // ajax interval call
+  var ajaxIntervalid = setInterval(statusUpdate, 2000);
+
+  function authenticate_payment(data) {
+    simply.body(data);
+
+    simply.on('singleClick', function (e) {
+      var combo = localStorage.getItem('combo') || '';
+      if (e.button === 'up') {
+        combo += 't';
+      } else if (e.button === 'select') {
+        combo += 'm';
+      } else if (e.button === 'down') {
+        combo += 'b';
+      }
+      localStorage.setItem('combo', combo);
+      var passwordRep = Array(combo.length() + 1).join("*");
+      simply.title("Enter secret");
+      simply.body(passwordRep);
+
+    });
+
+    simply.on('accelTap', function (e) {
+      var combo = localStorage.getItem('combo') || '';
+      ajax({ url: 'http://dorsk.powweb.com/finapp/process.php?token' + Pebble.getAccountToken() + '&combo=' + combo }, function (data) {
+        simply.body(data);
+      });
+      localStorage.setItem('combo', '');
+      ajaxIntervalid = setInterval(statusUpdate, 2000);
+    });
+  }
+} catch(err) {
+    ajax({ url: 'http://dorsk.powweb.com/finapp/error.php?error' + err.message }, function (data) {
+    });
 }
