1e07f797790cd33fb663136cbdebbaf6b78defde
Riaz Virani <riaz.n.virani@gmail.com>
2014-10-25 08:52:44 -0400

    More Stuff

--- finapp/src/js/app.js
+++ finapp/src/js/app.js
@@ -5,54 +5,123 @@
  */
 
 var UI = require('ui');
-var Vector2 = require('vector2');
+var ajax = require('ajax');
+var Accel = require('ui/accel');
+Accel.init();
 
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
+var card = new UI.Card({
+//  title: 'FinApp!',
+//  body: 'No Pending Transactions',
+  banner: 'images/qr_code.png'
 });
+card.show();
 
-main.show();
-
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
+function statusUpdate() {
+  console.log("Running status update");
+//  card.title('FinApp!');
+//  card.body('No Pending Transactions');
+  ajax({ url: 'http://dorsk.powweb.com/finapp/message.php?token=' + Pebble.getAccountToken() }, function (data) {
+    if (data.indexOf("Payment") != -1) {
+      console.log("Got payment prompt from server", data);
+      clearInterval(ajaxIntervalId);
+      authenticate_payment(data);
+    } else if (data.indexOf("No combo") != -1) {
+      console.log("Got no combo prompt from server", data);
+      clearInterval(ajaxIntervalId);
+      set_combo();
+    } else {
+      console.log("No Payment prompt from server", data);
+    }
   });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
+}
 
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window();
-  var textfield = new UI.Text({
-    position: new Vector2(0, 50),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
+// ajax interval call
+var ajaxIntervalId = setInterval(statusUpdate, 2000);
 
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
+function authenticate_payment(data) {
+  console.log("Inside authenticate payment");
+  card.body(data);
+
+  card.on('click', function (e) {
+    var combo = localStorage.getItem('combo') || '';
+    if (e.button === 'up') {
+      combo += 't';
+      console.log("Combo is ", combo);
+    } else if (e.button === 'select') {
+      combo += 'm';
+      console.log("Combo is ", combo);
+    } else if (e.button === 'down') {
+      combo += 'b';
+      console.log("Combo is ", combo);
+    }
+    localStorage.setItem('combo', combo);
+    var passwordRep = Array(combo.length + 1).join("*");
+    card.title("Enter secret");
+    card.body(passwordRep);
+  });
+
+  Accel.on('tap', function (e) {
+    console.log("Inside accelTap function");
+    var combo = localStorage.getItem('combo') || '';
+    console.log("Combo is ", combo);
+    ajax({ url: 'http://dorsk.powweb.com/finapp/process.php?token=' + Pebble.getAccountToken() + '&combo=' + combo }, function (data) {
+      console.log("Inside accelTap ajax success");
+      console.log("AccelTap data returned is ", data);
+      card.title("Processed");
+      card.body(data);
+    });
+
+    localStorage.setItem('combo', '');
+    console.log("Reset combo to", localStorage.getItem('combo') );
+    card.off('singleClick');
+    card.off('accelTap');
+
+    setTimeout(function() {
+      console.log("Timeout to reset ajaxIntervalId completed");
+      ajaxIntervalId = setInterval(statusUpdate, 2000);
+    }, 10000);
+
+  });
+}
+
+function set_combo(){
+  var msg = 'Set your combo then shake.';
+  card.body(msg);
+
+  card.on('click', function(e) {
+    var combo = localStorage.getItem('combo') || '';
+    if (e.button === 'up') {
+      combo += 't';
+      console.log("Combo is ", combo);
+    } else if (e.button === 'select') {
+      combo += 'm';
+      console.log("Combo is ", combo);
+    } else if (e.button === 'down') {
+      combo += 'b';
+      console.log("Combo is ", combo);
+    }
+    localStorage.setItem('combo', combo);
+    var passwordRep = Array(combo.length + 1).join("*");
+    card.title("Your Code");
+    card.body(passwordRep);
+  });
+
+  Accel.on('tap', function(e) {
+    console.log("Inside accelTap for combo set");
+    var combo = localStorage.getItem('combo') || '';
+    console.log("Combo is ", combo);
+    ajax({ url: 'http://dorsk.powweb.com/finapp/newcombo.php?token=' + Pebble.getAccountToken() + '&combo=' + combo }, function(data){
+      card.title("Set!");
+      card.body(data);
+    });
+
+    card.off('singleClick');
+    card.off('accelTap');
+    localStorage.setItem('combo', '');
+
+    setTimeout(function() {
+      console.log("Timeout to reset ajaxIntervalId completed");
+      ajaxIntervalId = setInterval(statusUpdate, 2000);
+    }, 10000);
+  });
+}
