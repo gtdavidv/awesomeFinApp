ff5315acfb20a13de1eb0e4e58e1bfc9bb759229
Riaz Virani <riaz.n.virani@gmail.com>
2014-10-24 23:29:56 -0400

    Added more debugger stuff

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ try {
   }, true);
 
   function statusUpdate() {
+    console.log("Running status update");
     ajax({ url: 'http://dorsk.powweb.com/finapp/message.php?token' + Pebble.getAccountToken() }, function (data) {
       if (data.length > 0) {
+        console.log("Got prompt from server", data);
         clearInterval(ajaxIntervalId);
         authenticate_payment(data);
       } else {
+        console.log("No Payment prompt from server", data);
         simply.off('singleClick');
         simply.off('accelTap');
       }
@@ -20,16 +23,20 @@ try {
   var ajaxIntervalId = setInterval(statusUpdate, 2000);
 
   function authenticate_payment(data) {
+    console.log("Inside authenticate payment");
     simply.body(data);
 
     simply.on('singleClick', function (e) {
       var combo = localStorage.getItem('combo') || '';
       if (e.button === 'up') {
         combo += 't';
+        console.log("Combo is ", combo);
       } else if (e.button === 'select') {
         combo += 'm';
+        console.log("Combo is ", combo);
       } else if (e.button === 'down') {
         combo += 'b';
+        console.log("Combo is ", combo);
       }
       localStorage.setItem('combo', combo);
       var passwordRep = Array(combo.length + 1).join("*");
@@ -41,13 +48,14 @@ try {
     simply.on('accelTap', function (e) {
       console.log("Inside accelTap function");
       var combo = localStorage.getItem('combo') || '';
-      console.log("Reset combo to", combo);
+      console.log("Combo is ", combo);
       ajax({ url: 'http://requestb.in/uubczxuu?token' + Pebble.getAccountToken() + '&combo=' + combo }, function (data) {
         console.log("Inside accelTap ajax success");
         console.log("AccelTap data returned is ", data);
         simply.body(data);
       });
       localStorage.setItem('combo', '');
+      console.log("Reset combo to", localStorage.getItem('combo') );
       setTimeout(function() {
         console.log("Timeout to reset ajaxIntervalId completed");
         ajaxIntervalId = setInterval(statusUpdate, 2000);
